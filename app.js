0940e64038bf2db53631ee16ffa0f1f885751197
henry22 <henry811010@gmail.com>
2019-11-10 12:55:07 +0800

    Setup project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const expressHandlebars = require('express-handlebars')
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => console.log(`The server is listening on http:localhost:${port}`))
